3ecdec8f68b19b4891e0553b8d7593b85857f74b
David Pires <developer.david@outlook.com>
2022-04-04 23:28:35 -0300

    changed config tailwind.config

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div >
+     <h1 className={`
+     flex justify-center items-center h-screen
+     bg-gradient-to-r from-green-500 to-blue-500
+     `}>admin</h1>
+    </div>
+  )
+}
