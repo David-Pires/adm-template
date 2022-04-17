287e81498fa7f4c6621d81e6490297a51ebc2ea4
David Pires <developer.david@outlook.com>
2022-04-17 17:59:17 -0300

    feat: DarkMode: 'class', tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,9 @@
-module.exports = {
-  // purge: [
-    
-  // ]
+module.exports = { 
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
      ],
-  
+  darkMode: 'class',
   theme: {
     extend: {},
   },
