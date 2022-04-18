fffe64ac3cc81a6aeed7f0f46321adec2f6aec2c
David Pires <developer.david@outlook.com>
2022-04-17 22:32:59 -0300

    feat: logo

--- src/components/template/MenuLateral.tsx
+++ src/components/template/MenuLateral.tsx
@@ -1,5 +1,6 @@
 import { IconeCasa, IconeAjustes, IconeSino } from "../icons";
 import MenuItem from "./MenuItem";
+import Logo from "./Logo"
 
 export default function MenuLateral() {
   return (
@@ -13,10 +14,12 @@ export default function MenuLateral() {
         from-indigo-500
         to-purple-800
         h-20
+        w-20
       `}>
-
+        <Logo />
       </div>
       <ul>
+        
         <MenuItem url="/" texto="Início" icone={IconeCasa} />
         <MenuItem url="/ajustes" texto="Ajustes" icone={IconeAjustes} />
         <MenuItem url="/notificacoes" texto="notificações" icone={IconeSino}/>
