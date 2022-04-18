050efdea5554d692f6d2e4f9dfc8eecd2ce64066
David Pires <developer.david@outlook.com>
2022-04-17 21:57:28 -0300

    feat:  background layout

--- src/components/template/MenuLateral.tsx
+++ src/components/template/MenuLateral.tsx
@@ -4,6 +4,18 @@ import MenuItem from "./MenuItem";
 export default function MenuLateral() {
   return (
     <aside>
+      <div className={`
+        flex
+        flex-col
+        items-center
+        justify-content
+        bg-gradient-to-r
+        from-indigo-500
+        to-purple-800
+        h-20
+      `}>
+
+      </div>
       <ul>
         <MenuItem url="/" texto="Início" icone={IconeCasa} />
         <MenuItem url="/ajustes" texto="Ajustes" icone={IconeAjustes} />
