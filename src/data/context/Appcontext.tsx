18da2af9e27d2fee08d18c6cfacbf0e8ab99b543
David Pires <developer.david@outlook.com>
2022-04-20 23:11:50 -0300

    feat: AppContext.Provider

--- src/data/context/Appcontext.tsx
+++ src/data/context/Appcontext.tsx
@@ -2,4 +2,13 @@ import { createContext } from 'react';
 
 const AppContext = createContext({})
 
+export function AppProvider(props) {
+  return (
+    <AppContext.Provider value={{
+      nome: 'Teste Context API'
+    }}>
+
+    </AppContext.Provider>
+  )
+}
 export default AppContext
