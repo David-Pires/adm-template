283ae2a23b2131cf5e43888fe1c12a874ec8274d
David Pires <developer.david@outlook.com>
2022-04-21 00:07:14 -0300

    feat: alternar tema dark

--- src/components/template/Cabecalho.tsx
+++ src/components/template/Cabecalho.tsx
@@ -1,4 +1,5 @@
-import MenuLateral from './MenuLateral'
+import useAppData from '../../data/hook/useAppData'
+import BotaoAlternarTema from './BotaoAlternarTema'
 import Titulo from './Titulo'
 
 interface CabecalhoProps {
@@ -7,11 +8,21 @@ interface CabecalhoProps {
   
 }
 export default function Cabecalho(props: CabecalhoProps) {
+  const {tema, alternarTema} = useAppData()
   return (
-    <div>
+    <div className={`
+      flex
+    `}>
       <Titulo titulo={props.titulo} 
               subtitulo={props.subtitulo}
       />
+      <div className={`
+        flex
+        flex-grow
+        justify-end
+      `}>
+        <BotaoAlternarTema tema={tema} alternarTema={alternarTema}/>
+      </div>
       
     </div>
   )
