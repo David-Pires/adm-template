6a97b01014c20c88f0a460bb1aacd5dfd48858e4
David Pires <developer.david@outlook.com>
2022-04-23 03:41:54 -0300

    fix: import AuthContext

--- src/data/context/AuthContext.tsx
+++ src/data/context/AuthContext.tsx
@@ -4,24 +4,23 @@ import firebase from '../../firebase/config'
 import Usuario from '../../model/Usuario'
 
 interface AuthContextProps {
-  usuario: Usuario
+  usuario?: Usuario
   loginGoogle: () => Promise<void>
 }
 const AuthContext = createContext<AuthContextProps> ({})
 
-async function usuarioNormalizado(usuarioFirebase: firebase.User):
-Promise<Usuario> {
-  const token = await usuarioFirebase.getIdToken()
-  return {
-    uid: usuarioFirebase.uid,
-    nome: usuarioFirebase.displayName,
-    email: usuarioFirebase.email,
-    token,
-    provedor: usuarioFirebase.providerData[0].providerId,
-    imagemUrl: usuarioFirebase.photoURL
-  }
-}
-export function AuthProvider(props) {
+// async function usuarioNormalizado(usuarioFirebase: firebase.User):Promise<Usuario> {
+//   const token = await usuarioFirebase.getIdToken()
+//   return {
+//     uid: usuarioFirebase.uid,
+//     nome: usuarioFirebase.displayName,
+//     email: usuarioFirebase.email,
+//     token,
+//     provedor: usuarioFirebase.providerData[0].providerId,
+//     imagemUrl: usuarioFirebase.photoURL
+//   }
+// }
+export  function AuthProvider(props) {
   const [usuario, setUsuario] = useState<Usuario>(null)
 
   async function loginGoogle() {
