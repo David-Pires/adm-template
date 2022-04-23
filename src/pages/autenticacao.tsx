fd7cb4336ffc4e2042327ef58764402a7c81ffd9
David Pires <developer.david@outlook.com>
2022-04-22 21:38:12 -0300

    feat: button para autenticacao

--- src/pages/autenticacao.tsx
+++ src/pages/autenticacao.tsx
@@ -5,9 +5,21 @@ export default function autenticacao() {
   const [modo, setModo] = useState<'login' | 'cadastro'>('login')
   const [email, setEmail] = useState('')
   const [senha, setSenha] = useState('')
+
+  function submeter() {
+    if(modo === 'login') {
+      console.log('login')
+    } else {
+      console.log('cadastrar')
+    }
+  }
   return (
     <div>
-      <h1>Autenticacao</h1>
+      <h1 className={`
+        text-xl font-bold mb-5
+      `}>
+        {modo === 'login' ? 'Entre com a Sua Conta' : 'Cadastre-se na Plataforma'}
+      </h1>
       <AuthInput
         label="Email"
         tipo="email"
@@ -22,6 +34,17 @@ export default function autenticacao() {
         valorMudou={setSenha}
         obrigatorio
       />      
+      <button onClick={submeter} className={`
+        w-full bg-indigo-500 hover:bg-indigo-400
+        text-white rounded-lg px-4 py-3 mt-6
+      `}>
+      {modo === 'login' ? 'Entrar' : 'Cadastrar'}
+
+      </button>
+      
+      <hr className={`
+        my-6 border-gray-300 w-full
+      `}/>
     </div>
   )
 }
