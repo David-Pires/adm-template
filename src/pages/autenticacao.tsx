e5c10b48c22fbbd3607aa942da6d7ad1aa9ec3da
David Pires <developer.david@outlook.com>
2022-04-22 23:28:46 -0300

    feat: tela de login de login e cadastro

--- src/pages/autenticacao.tsx
+++ src/pages/autenticacao.tsx
@@ -14,11 +14,16 @@ export default function autenticacao() {
     }
   }
   return (
-    <div className="flex flex-col h-screen items-center justify-center">
-
-      <div className="w-1/2">
+    <div className="flex  h-screen items-center justify-center">
+      <div className="hidden md:block md:w-1/2 lg:w-2/3">
+        <img 
+          src="https://source.unsplash.com/random" 
+          alt="Imagem da Tela de Autenticação"
+          className="h-screen w-full object-cover" />
+      </div>
+      <div className="m-10 w-full md:w-1/2">
         <h1 className={`
-          text-xl font-bold mb-5
+          text-2xl font-bold mb-5
         `}>
           {modo === 'login' ? 'Entre com a Sua Conta' : 'Cadastre-se na Plataforma'}
         </h1>
@@ -51,9 +56,27 @@ export default function autenticacao() {
             w-full bg-red-500 hover:bg-red-400
             text-white rounded-lg px-4 py-3 mt-6
           `}>
-          {modo === 'login' ? 'Entrar com Google' : 'Cadastrar'}
-              
+              Entrar com Google
         </button> 
+        {modo ==='login' ? (
+            <p className="mt-8">
+              Novo por aqui?
+              <a onClick={() => setModo('cadastro')} className={`
+                text-blue-500 hover:text-blue-700
+                cursor-pointer
+              `}> Criar uma conta gratuitamente </a>
+            </p> 
+
+        ) : (
+          <p className="mt-8">
+              Já faz parte da nossa comunidade?
+                 <a onClick={() => setModo('login')} className={`
+                  text-blue-500 hover:text-blue-700 font-semibold
+                  cursor-pointer
+                  `}> Entre com as suas credenciais
+                </a>
+          </p>
+        )}
       </div>
     </div>
   )
