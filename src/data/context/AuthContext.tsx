ca675f50b6c4c6451135ef10537a645d6cfd3d2a
David Pires <developer.david@outlook.com>
2022-04-23 22:55:15 -0300

    feat: logout

--- src/data/context/AuthContext.tsx
+++ src/data/context/AuthContext.tsx
@@ -53,12 +53,28 @@ export  function AuthProvider(props) {
   }
 
   async function loginGoogle() {
-    const resp = await firebase.auth().signInWithPopup(
-      new firebase.auth.GoogleAuthProvider()
-    )   
+    try {
+      setCarregando(true)
+      const resp = await firebase.auth().signInWithPopup(
+        new firebase.auth.GoogleAuthProvider()
+      )   
+  
+        configurarSessao(resp.user)
+        route.push('/')  
+    }  finally {
+      setCarregando(false)
+    }
+  }
 
-      configurarSessao(resp.user)
-      route.push('/')    
+  async function logout() {
+    try {
+      setCarregando(true)
+      await firebase.auth().signOut()
+      await configurarSessao(null)
+    } finally {
+      setCarregando(false)
+    }
+
   }
   useEffect(() => {
      const cancelar =  firebase.auth().onIdTokenChanged(configurarSessao)
